a47a9b63eb5211a32049b477f8bbd8d1eac45b6d
megmgleason <marymargaretgleason@gmail.com>
2022-09-21 16:48:33 -0400

    Initial Commit

---
+++ SwiftUIState/SwiftUIStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateApp.swift
+//  SwiftUIState
+//
+//  Created by MEG G on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
